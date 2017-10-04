40277d25ff959dc84026a1306fffef3a30ffd6ec
Dennis Li <djli1996@gmail.com>
2017-10-04 16:04:00 -0700

    slurm file

---
+++ runMpiVINA.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#SBATCH -N 2
+#SBATCH -n 12
+#SBATCH -c 6
+#SBATCH -t 0-23:00:00
+#SBATCH -o testrun.out
+#SBATCH -e testrun.err
+
+#Compile the application.
+ls > ligandlist ./Ligand
+mkdir -p Output
+mkdir -p ProcessedLigand
+make
+
+#Run the application.
+echo "MPI-Vina is running..."
+mpirun mpiVINA
+echo "Processing has finished"
+echo "See the MpiVina.log file."
+
+#Result analysis.
+echo "Analysizing the results..."
+cd Output
+grep "  1 " *.txt | cut -c1-12,35-42 > result 
+#grep "  1 " *.txt | cut -c1-7,30-35 > result
+echo "See the 'result' file in the 'Output' directory."
+#cat result
+
+echo "Sorting the results..."
+sort -n +1 -2 result -o SortedResult
+echo "See the 'SortedResult' file in the 'Output' directory."
+#cat SortedResult
