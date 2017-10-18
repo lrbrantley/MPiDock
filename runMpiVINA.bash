4290fa217ebf6216c0e70cc33c66159c5343b634
Dennis Li <djli1996@gmail.com>
2017-10-18 16:57:28 -0700

    Still working on modifying bash script for Dr.E

--- runMpiVINA.bash
+++ runMpiVINA.bash
@@ -22,7 +22,11 @@ echo "See the MpiVina.log file."
 #Result analysis.
 echo "Analysizing the results..."
 cd Output
-grep "  1 " *.txt | cut -c1-12,35-42 > result 
+for f in inh*.txt; do
+   grep -H "  1" $f | # grab top result with file name.
+   awk '{print $1" "$3}' | # get first col (name) and third col (energy)
+   sed 's/://' # remove colon from file name
+   >> unsorted.txt
 #grep "  1 " *.txt | cut -c1-7,30-35 > result
 echo "See the 'result' file in the 'Output' directory."
 #cat result
