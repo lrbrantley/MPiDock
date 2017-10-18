4ad61a12860adba7d106c32c0c3f645683c43bc8
Greycrazygamer <dnola@calpoly.edu>
2017-10-18 16:19:42 -0700

    Add 127 Bash

---
+++ mpi-vina-master/src/runMpiVINA127.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+#This is the script for running the application by providing -np parameter from terminal.
+
+#Compile the application.
+ls > ligandlist ./Ligand
+mkdir -p Output
+mkdir -p ProcessedLigand
+make
+
+STATE= plm_rsh_no_tree_spawn 1
+HOST=host_file
+SOURCE= /usr/lib64/openmpi/bin/mpirun
+#Run the application.
+echo "MPI-Vina is running..."
+$SOURCE --mca $STATE -hostfile $HOST -np 4 mpiVINA > Output/MpiVina.log
+echo "Processing has finished"
+echo "See the MpiVina.log file."
+
+#Result analysis.
+echo "Analysizing the results..."
+cd Output
+grep "  1 " *.txt | cut -c1-12,35-42 > result 
+#grep "  1 " *.txt | cut -c1-7,30-35 > result
+echo "See the 'result' file in the 'Output' directory."
+#cat result
+
+echo "Sorting the results..."
+sort -n +1 -2 result -o SortedResult
+echo "See the 'SortedResult' file in the 'Output' directory."
+#cat SortedResult
