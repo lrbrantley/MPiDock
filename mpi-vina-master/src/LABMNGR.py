73285497235facee498ccc38a8d7a7878c6ba3cc
Derek Nola <dnola@calpoly.edu>
2017-10-30 10:36:24 -0700

    small ssh update

--- mpi-vina-master/src/LABMNGR.py
+++ mpi-vina-master/src/LABMNGR.py
@@ -3,7 +3,7 @@ from os import system, makedirs, chdir
 import subprocess
 import sys
 import argparse
-
+import platform
 
 parser = argparse.ArgumentParser(description='Launch MPI-VINA across Lab 127 cluster')
 parser.add_argument('-r', '--reset', action='store_true',  default=False,
@@ -19,6 +19,34 @@ args = parser.parse_args()
 def verbose_print(string):
     if args.verbose:
         print(string)
+
+
+def setup_script():
+	user = input("What is your ssh username?: ")
+	hostname = platform.node()
+	verbose_print("Hostname found:" + hostname)
+	verbose_print("Setting up connections with Lab 127 Nodes")
+	alive_nodes = list()
+	for i in range(10, 37):
+		cmd = "127x" + str(i) + ".csc.calpoly.edu"
+        val = 0
+        try:
+            val = subprocess.check_output(['ping', '-c 1', cmd])
+        except subprocess.CalledProcessError as e:
+            continue
+        if val is not 0:
+        	alive_nodes.append(cmd)
+
+    for node in alive_nodes:
+    	system("ssh-keygen -R " + hostname)
+    	system("ssh-keygen -R " + node)
+    	system("ssh-keygen -H " + node + " >> ~/.ssh/known_hosts")
+    	system("ssh-keygen -H " + hostname + " >> ~/.ssh/known_hosts")
+    verbose_print("Successful setup ssh keys with:")
+    for node in alive_nodes: verbose_print(node)
+
+
+	verbose_print("Setup Complete")
 
 
 def rewrite_lab_state(num_nodes):
@@ -38,6 +66,7 @@ def rewrite_lab_state(num_nodes):
     if hoster in good_nodes:
         good_nodes.remove(hoster)
     good_nodes.insert(0, hoster)
+
     with open(args.hostfile, 'w') as hosts:
         for node in good_nodes:
             hosts.write(node + " slots=2" + '\n')
@@ -45,6 +74,12 @@ def rewrite_lab_state(num_nodes):
 
 def main():
     print ("This is a Lab 127 Impromptu Cluster Creator/Manager")
+    inital = '~'
+    while( inital is not 'y' and inital is not 'n'):
+    	inital = input("Is this the first time running this script on this machine (y/n)?: ")
+    if inital is 'y':
+    	setup_script()
+
     num_nodes = 0
     while( num_nodes < 1 or num_nodes > 26 ):
         inputs = input ("Enter the number of machines you wish to use (1 to 26): ")
