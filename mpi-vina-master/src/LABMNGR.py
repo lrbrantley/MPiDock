863b29236f07dd81a0139e34ba0372ddc46ab331
Greycrazygamer <dnola@calpoly.edu>
2017-10-30 12:00:39 -0700

    Change from mpirun to mpiexec

--- mpi-vina-master/src/LABMNGR.py
+++ mpi-vina-master/src/LABMNGR.py
@@ -1,134 +1,147 @@
-#!/bin/python3
-from os import system, makedirs, chdir
+#!/usr/bin/python3
+from os import system, makedirs, chdir, path
 import subprocess
 import sys
 import argparse
 import platform
 
+
 parser = argparse.ArgumentParser(description='Launch MPI-VINA across Lab 127 cluster')
-parser.add_argument('-r', '--reset', action='store_true',  default=False,
-                    help='rewrite host_file with currently active nodes')
 parser.add_argument('--hostfile', action='store', default="./host_file",
-                    help='change which hostfile is used')
+					help='change which hostfile is used')
 parser.add_argument('-v', '--verbose', action='store_true',
-                    help='print verbose output')
+					help='print verbose output')
 parser.add_argument('-t', '--test', action='store_true',
-                    help='launch test application to verify mpi connections')
+					help='launch test application to verify mpi connections')
 args = parser.parse_args()
 
 def verbose_print(string):
-    if args.verbose:
-        print(string)
+	if args.verbose:
+		print(string)
 
 
 def setup_script():
 	user = input("What is your ssh username?: ")
 	hostname = platform.node()
 	verbose_print("Hostname found:" + hostname)
+	ssh_path = path.expanduser("~/.ssh/")
+	if not path.exists(ssh_path + "id_rsa.pub"):
+		print("ERROR: public ssh key at" + "does not exist, please generate one")
+		print("Run:ssh-keygen")
+		exit(1)
+
 	verbose_print("Setting up connections with Lab 127 Nodes")
 	alive_nodes = list()
+	# Get health of Lab 127 and find all active nodes
 	for i in range(10, 37):
+		# lab machines 31 and 37 don't have intel mpi installed
+		if i is 31 or i is 37:
+			continue
 		cmd = "127x" + str(i) + ".csc.calpoly.edu"
-        val = 0
-        try:
-            val = subprocess.check_output(['ping', '-c 1', cmd])
-        except subprocess.CalledProcessError as e:
-            continue
-        if val is not 0:
-        	alive_nodes.append(cmd)
+		val = 0
+		try:
+			val = subprocess.check_output(['ping', '-c 1', cmd])
+		except subprocess.CalledProcessError as e:
+			continue
+		if val is not 0:
+			alive_nodes.append(cmd)
 
-    for node in alive_nodes:
-    	system("ssh-keygen -R " + hostname)
-    	system("ssh-keygen -R " + node)
-    	system("ssh-keygen -H " + node + " >> ~/.ssh/known_hosts")
-    	system("ssh-keygen -H " + hostname + " >> ~/.ssh/known_hosts")
-    verbose_print("Successful setup ssh keys with:")
-    for node in alive_nodes: verbose_print(node)
+	# add nodes to known_hosts file
+	for node in alive_nodes:
+		system("ssh-keygen -R " + node)
+		system("ssh-keyscan -H " + node + " >> ~/.ssh/known_hosts")
+		# setup public ssh key login
+		system("ssh-copy-id -i ~/.ssh/id_rsa.pub " + user + "@" + node)
+
+	verbose_print("Successful setup ssh keys with:")
+	for node in alive_nodes: verbose_print(node)
 
 
 	verbose_print("Setup Complete")
 
 
 def rewrite_lab_state(num_nodes):
-    good_nodes = list()
-    for i in range(10, 9 + num_nodes):
-        cmd = "127x" + str(i) + ".csc.calpoly.edu"
-        val = 0
-        try:
-            val = subprocess.check_output(['ping', '-c 2', cmd])
-        except subprocess.CalledProcessError as e:
-            continue
-        if val is not 0:
-            good_nodes.append(cmd)
-    hoster = subprocess.check_output(['hostname'])
-    hoster = str(hoster, 'utf-8')
-    hoster = hoster.rstrip('\n')
-    if hoster in good_nodes:
-        good_nodes.remove(hoster)
-    good_nodes.insert(0, hoster)
+	good_nodes = list()
+	for i in range(10, 9 + num_nodes):
+		# lab machines 31 and 37 don't have intel mpi installed
+		if i is 31 or i is 37:
+			continue
+		cmd = "127x" + str(i) + ".csc.calpoly.edu"
+		val = 0
+		try:
+			val = subprocess.check_output(['ping', '-c 1', cmd])
+		except subprocess.CalledProcessError as e:
+			continue
+		if val is not 0:
+			good_nodes.append(cmd)
+	hoster = subprocess.check_output(['hostname'])
+	hoster = str(hoster, 'utf-8')
+	hoster = hoster.rstrip('\n')
+	if hoster in good_nodes:
+		good_nodes.remove(hoster)
+	good_nodes.insert(0, hoster)
 
-    with open(args.hostfile, 'w') as hosts:
-        for node in good_nodes:
-            hosts.write(node + " slots=2" + '\n')
-    return good_nodes
+	with open(args.hostfile, 'w') as hosts:
+		for node in good_nodes:
+			hosts.write(node + " slots=2" + '\n')
+	return good_nodes
 
 def main():
-    print ("This is a Lab 127 Impromptu Cluster Creator/Manager")
-    inital = '~'
-    while( inital is not 'y' and inital is not 'n'):
-    	inital = input("Is this the first time running this script on this machine (y/n)?: ")
-    if inital is 'y':
-    	setup_script()
+	print ("This is a Lab 127 Impromptu Cluster Creator/Manager")
+	inital = '~'
+	while( inital is not 'y' and inital is not 'n'):
+		inital = input("Is this the first time running this script on this machine (y/n)?: ")
+	if inital is 'y':
+		setup_script()
 
-    num_nodes = 0
-    while( num_nodes < 1 or num_nodes > 26 ):
-        inputs = input ("Enter the number of machines you wish to use (1 to 26): ")
-        num_nodes = int(inputs)
+	num_nodes = 0
+	while( num_nodes < 1 or num_nodes > 26 ):
+		inputs = input ("Enter the number of machines you wish to use (1 to 26): ")
+		num_nodes = int(inputs)
 
-    if args.reset:
-        alive = rewrite_lab_state(num_nodes)
-        print("Using Nodes:");
-        for n in alive:
-            print(n)
+	print("Using Nodes:");
+	alive = rewrite_lab_state(num_nodes)
+	for n in alive:	print(n)
 
-    system("ls > ligandlist ./Ligand")
+	system("ls > ligandlist ./Ligand")
 
-    makedirs(name="./Output", exist_ok=True)
-    makedirs(name="./ProcessedLigand", exist_ok=True)
+	makedirs(name="./Output", exist_ok=True)
+	makedirs(name="./ProcessedLigand", exist_ok=True)
 
-    system("make")
+	system("make")
 
-    mpi_source = "/usr/lib64/openmpi/bin/mpirun "
-    mpi_args = "--mca plm_rsh_no_tree_spawn 1"
-    mpi_args += " -np " + str(num_nodes)
-    mpi_args += " --map-by node "
-    mpi_args += " -hostfile " + args.hostfile
-    if args.test:
-    	mpi_out = " mpi_hello_world"
-    else:
-    	if args.verbose:
-    		mpi_out = " mpiVINA | tee Output/MpiVina.log"
-    	else:
-    		mpi_out = " mpiVINA > Output/MpiVina.log"
+	mpi_source = "mpiexec "
+	#mpi_args = "--mca plm_rsh_no_tree_spawn 1"
+	mpi_args += "--prefix /usr/lib64/openmpi/"
+	mpi_args += " --map-by ppr:1:node" 
+	mpi_args += " -display-map"
+	mpi_args += " -hostfile " + args.hostfile
+	if args.test:
+		mpi_out = " mpi_hello_world"
+	else:
+		if args.verbose:
+			mpi_out = " mpiVINA | tee Output/MpiVina.log"
+		else:
+			mpi_out = " mpiVINA > Output/MpiVina.log"
 
-    verbose_print(mpi_source + mpi_args + mpi_out)
+	verbose_print(mpi_source + mpi_args + mpi_out)
 
-    print("MPI-Vina is running...")
-    
-    system(mpi_source + mpi_args + mpi_out)
+	print("MPI-Vina is running...")
+	
+	system(mpi_source + mpi_args + mpi_out)
 
-    print("Processing has finished")
-    print("See the MpiVina.log file.")
+	print("Processing has finished")
+	print("See the MpiVina.log file.")
 
-    #Result analysis.
-    print("Analysizing the results...")
+	#Result analysis.
+	print("Analysizing the results...")
 
-    chdir("Output")
-    system("grep \"  1 \" *.txt | cut -c1-12,35-42 > result ")
+	chdir("Output")
+	system("grep \"  1 \" *.txt | cut -c1-12,35-42 > result ")
 
-    print("See the 'result' file in the 'Output' directory.")
-    print("Sorting the results...")
-    system("sort -n +1 -2 result -o SortedResult")
-    print("See the 'SortedResult' file in the 'Output' directory.")
+	print("See the 'result' file in the 'Output' directory.")
+	print("Sorting the results...")
+	system("sort -n +1 -2 result -o SortedResult")
+	print("See the 'SortedResult' file in the 'Output' directory.")
 
 main()
