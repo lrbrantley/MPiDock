9ecf9160c8531b73ff4fe38123a017ac42193f7b
Dennis Li <djli1996@gmail.com>
2017-11-06 15:24:16 -0800

    Fixing preprocess.

--- mpi-vina-master/src/mpiVinaEagon.bash
+++ mpi-vina-master/src/mpiVinaEagon.bash
@@ -7,6 +7,14 @@
 #SBATCH -o testrun.out
 #SBATCH -e testrun.err
 
+cleanInputFile ()
+{
+   local FILE=$1;
+   local ZINCID=$(grep "ZINC" $FILE |   # Move ZINC Ids to a REMARK, not USER, line.
+                  sed 's/USER/REMARK/');
+   sed -i -e "1i $ZINCID" -e '/USER/d' -e '/TER/d' $FILE;
+}
+
 #Copy over and Ensure all inh_ files don't have unusable things.
 cd Ligand
 for f in ligand*.pdbqt; do # Move files in parallel.
@@ -14,7 +22,7 @@ for f in ligand*.pdbqt; do # Move files in parallel.
 done
 wait # Join sub processes.
 for f in inh*.pdbqt; do    # Edit files in parallel.
-   sed -i -e '/USER/d' -e '/TER/d' $f &
+   cleanInputFile $f &
 done
 wait # Join sub processes.
 cd ..
