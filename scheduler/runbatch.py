75fdf2ea86a390a8d56b8e52173f1abb53976fd6
Dennis Li <djli1996@gmail.com>
2018-01-27 14:11:34 -0800

    more wip

--- scheduler/runbatch.py
+++ scheduler/runbatch.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+from os import listdir
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('command', help='Location of Command to execute remotely')
-parser.add_argument('user', help='User for ssh session')
-parser.add_argument('ssh', help='Remote SSH Address')
+parser.add_argument('ssh', help='User and Remote SSH Address or an alias')
 parser.add_argument('remote-path', help='Location on remote machine')
 parser.add_argument('-i', '--input',
         help='Location for input files. These will be transfered')
@@ -15,19 +15,21 @@ parser.add_argument('-b', '--batch',
 parser.add_argument('-to', '--timeout',
         help='Time limit for this job (and the remote job) to run. If the batch setting is\
                 used as well, batches will be run until time runs out.')
-parser.add_argument('-c', '--compress', default=True,
-        help='Compress files for transfer. Defaults to True.')
 parser.add_argument('-args', '--arguments',
         help='Arguments to send to remote command')
 parser.add_argument('-mdir', '--miscdir', help='Location of extra directory to send over')
 
 def scpPath():
-    return args.user + '@' + args.ssh + ':' + args.remote-path
+    return args.ssh + ':' + args.remote-path
 
 def main():
     args = parser.parse_args()
     command = args.command
     
+    inputP = args.input
+    if inputP is not None:
+        inputFiles = listdir(inputP)
+
 
 
 
