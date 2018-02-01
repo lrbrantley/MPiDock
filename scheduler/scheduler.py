66c6a4f95a84f61ce31d32654f9b644c952d43cf
Dennis Li <djli1996@gmail.com>
2018-01-31 21:20:18 -0800

    scheduler done, but not tested

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -17,22 +17,27 @@ VALID_OPTIONS = ["1", "2", "3", "4", "5"]
 
 JOBS_FILE = "scheduledjobs.txt"
 
+
 def readJobFile():
     with open(JOBS_FILE, "a+") as f:
         f.seek(0)
         return f.read()
 
+
 def parseJobFile():
     jobText = readJobFile().split("\n")
     return [jobFromJSON(x) for x in jobText if x]
+
 
 def writeJobFile(s):
     with open(JOBS_FILE, "w") as f:
         f.write(s)
 
+
 def writeJobsToJobFile(jl):
     s = '\n'.join([j.toJSON for j in jl]) + '\n'
     writeJobFile(s)
+
 
 def processOption():
     choice = ""
@@ -59,25 +64,37 @@ def performList():
         print('Job ' + str(i) + ': ' + str(j))
         i += 1
 
+
+def getValidHour():
+    hour = -1
+    while hour < 0 or hour > 23:
+        hour = input('Please input a time between 0 and 23\n').strip()
+    return int(hour)
+
+
+def getEndTime():
+    inputS = input("Which hour (0 - 23) would you like the job to end? -1 to run to completion\n").split()
+    end = int(inputS)
+
+
+def calculateTimeout(start, end):
+    if 0 <= end <= 23:
+        if end < start:
+            end += 24
+        return end - start   
+    else:
+        return None
+
+
 ## handleCreate creates a new job.
 ## Internally it should create a new job object, then place it into the crontab
 ##   and the JOBS_FILE
 def handleCreate():
-    start = -1
-    while (start < 0 or start > 23):
-        inputS = input("Which hour (0 - 23) would you like to start?\n")
-        start = int(inputS)
-
+    start = getValidHour()
     print("Start set to " + str(start))
 
-    inputS = input("Which hour (0 - 23) would you like the job to end? -1 to run to completion\n")
-    end = int(inputS)
-    timeout = None
-    if 0 <= end <= 23:
-        if end < start:
-            end += 24
-        timeout = end - start
-
+    end = getEndTime()
+    timeout = calculateTimeout(start, end)
     if timeout == None:
         print("Job set to run until completion\n")
     else:
@@ -94,7 +111,8 @@ def handleCreate():
     writeJobFile(currentJobs)
     writeJobToCrontab(job)
     performList()
-    
+   
+
 def writeToCrontab(cronStr):
     process = subprocess.Popen(['crontab', '-'], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     stdOutErr = process.communicate(bytes(newCrontab, 'utf-8'))
@@ -140,39 +158,89 @@ def deleteJobFromCrontab(crontab, job):
     newCrontab = '\n'.join(crontab) + '\n'
     writeToCrontab(newCrontab)
 
-def handleDelete():
-    currentCrontab = None
-    try:
-        currentCrontab = subprocess.check_output(['crontab', '-l']).decode()
-    except subprocess.CalledProcessError as e:
-        print('There are no jobs in the crontab\n')
-        return
-    cronLines = currentCrontab.split('\n')
 
-    currentJobs = parseJobFile()
+## Reads out existing jobs, and lets the user choose from them. Returns the Job chosen.
+def chooseFromExistingJobs(currentJobs):
     if not currentJobs:
         print("There are no existing jobs\n")
         return
+
     numJobs = len(currentJobs)
 
     choice = None
     while not choice:
         print('Which existing job would you like to delete?\n')
+        print('Choose a job below or type \'quit\'\n')
         choice = input(performList())
-        if (choice <= 0 || choice > numJobs)
+        if choice.lower() == 'quit':
+            return
+        numChoice = int(choice)
+        if (numChoice <= 0 || numChoice > numJobs)
             print('Job chosen does not exist\n')
             choice = None
 
-    jobToDelete = currentJobs[choice - 1]
+    return (currentJobs[numChoice - 1], numChoice - 1)
+
+
+def handleDelete():
+    currentJobs = parseJobFile()
+    jobToDelete, choice = chooseFromExistingJobs(currentJobs)
+    if not jobToDelete:
+        return
+
+    currentCrontab = readCrontab()
+    if not currentCrontab:
+        print('Crontab is empty. Nothing to delete.\n')
+        return
+
     deleteJobFromCrontab(currentCrontab, jobToDelete)
-    currentJobs.del(choice - 1)
+    currentJobs.del(choice)
     writeJobsToJobFile(currentJobs)
     performList()
 
 
+def getModifications(job):
+    print('Job\'s current settings: ' + str(job))
+    while true:
+        choice = input(('Choose what you would like to modify or type \'quit\'.\n'
+                        '1. Start Time\n'
+                        '2. End Time\n')).strip()
+        if choice == '1':
+            time = getValidHour()
+            job.start = time
+        elif choice == '2':
+            end = getEndTime()
+            timeout = calculateTimeout(job.start, end)
+            job.duration = timeout
+        elif choice == 'quit':
+            return
+        else:
+            print('invalid option entered')   
+
+
+def modifyJobInCrontab(crontab, job):
+    wsSplitCrontab = [x.split() for x in crontab]
+    jobLine = findJobFromCrontab(wsSplitCrontab, job.jobId)
+    crontab[jobLine] = job.cronStr() + '\n'
+    newCrontab = '\n'.join(crontab) + '\n'
+    writeToCrontab(newCrontab)
+
+
 def handleModify():
-    
-    print("not yet implemented")
+    currentJobs = parseJobFile()
+    jobToModify, choice = chooseFromExistingJobs(currentJobs)
+    if not jobToModify:
+        return
+
+    currentCrontab = readCrontab()
+    if not currentCrontab:
+        print('Crontab is empty. Nothing to delete.\n')
+        return
+
+    getModifications(job)
+    modifyJobInCrontab(currentCrontab, job)
+    currentJobs[choice] = job
+    performList()
 
 
 def main():
