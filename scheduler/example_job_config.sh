f5375b3bb5b820266fb0b1ed735ef3247e541c60
Dennis Li <djli1996@gmail.com>
2018-02-05 19:35:19 -0800

    example modified to work on laptop... still need to testtttt

--- scheduler/example_job_config.sh
+++ scheduler/example_job_config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # This is where you have the runbatch.py python script
-run_batch_loc='~/bin/runbatch.py'
+run_batch_loc=~/disdrugdisc/scheduler/runbatch.py
 
 # This is the LOCAL location of the command to run.
-my_cmd='~/zika/run_labmngr.sh'
+my_cmd=~/zika/run_labmngr.sh
 # This is the directory of extra files to send over.
 # Inside of it we'll place in the LABMNGR, mpiVina, Autodock Vina, receptor molecule, etc.
-misc_dir='~/zika/Vina'
+misc_dir=~/zika/Vina
 
 # This is an ssh alias, you could be explicit with 'user@remote_address' as well.
 my_ssh='javalab' 
@@ -15,12 +15,12 @@ my_ssh='javalab'
 remote_dir='~/zika_processing' 
 
 # This is the LOCAL location of the Input Ligands
-input='~/zika/Ligands'
+input=~/zika/Ligands
 # This is the LOCAL location of where Output will be transferred back to.
-output='~/zika/Output'
+output=~/zika/Output
 
 # This is the setting for how many to batch at once. Recommended is ~25k.
-batch_size=25000
+batch_size=50
 # Time limit in HOURS for when to stop sending batches.
 timelimit=8
 
